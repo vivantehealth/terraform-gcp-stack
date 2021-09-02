349244259e17a2a16f19b5804bb250bb7eeaa14c
Reese Wilson <3253971+figadore@users.noreply.github.com>
2021-09-02 14:14:49 -0700

    rename outputs and add more

--- outputs.tf
+++ outputs.tf
@@ -2,16 +2,24 @@ output "repo" {
   value = var.repo
 }
 
-output "repo_plan_sa" {
+output "repo_plan_sa_email" {
   value = google_service_account.terraform_planner.email
+}
+
+output "repo_plan_sa_id" {
+  value = google_service_account.terraform_planner.id
 }
 
 output "repo_plan_environment" {
   value = github_repository_environment.repo_plan_environment.environment
 }
 
-output "repo_apply_sa" {
+output "repo_apply_sa_email" {
   value = google_service_account.terraformer.email
+}
+
+output "repo_apply_sa_id" {
+  value = google_service_account.terraformer.id
 }
 
 output "repo_apply_environment" {
