f8957267d66798fdb710b677cb5795c387fbdc60
Reese Wilson <3253971+figadore@users.noreply.github.com>
2021-09-09 18:01:59 -0700

    curl fail with body, remove debug

--- main.tf
+++ main.tf
@@ -71,22 +71,9 @@ resource "null_resource" "terraformer_membership" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<-EOT
-      echo '${jsonencode(var.group_roles)}'
-    EOT
-  }
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<-EOT
-      echo '${jsonencode(var.group_roles)}' | jq '[.[] | {name: .}]'
-    EOT
-  }
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<-EOT
       set -eo pipefail
       members=$(echo '${jsonencode(var.group_roles)}' | jq '[.[] | {name: .}]')
-      echo curl -H 'Authorization: Bearer $(gcloud auth print-access-token)' -X POST -d '{"roles": $members, "preferredMemberKey": { "id": "${google_service_account.terraformer.email}" } }' https://cloudidentity.googleapis.com/v1beta1/${var.terraformers_google_group_id}/memberships"
-      curl -H 'Authorization: Bearer $(gcloud auth print-access-token)' -X POST -d '{"roles": $members, "preferredMemberKey": { "id": "${google_service_account.terraformer.email}" } }' https://cloudidentity.googleapis.com/v1beta1/${var.terraformers_google_group_id}/memberships"
+      curl --fail-with-body -H 'Authorization: Bearer $(gcloud auth print-access-token)' -X POST -d '{"roles": $members, "preferredMemberKey": { "id": "${google_service_account.terraformer.email}" } }' https://cloudidentity.googleapis.com/v1beta1/${var.terraformers_google_group_id}/memberships"
     EOT
   }
 }
@@ -97,8 +84,7 @@ resource "null_resource" "terraform_planner_membership" {
     command     = <<-EOT
       set -eo pipefail
       members=$(echo '${jsonencode(var.group_roles)}' | jq '[.[] | {name: .}]')
-      echo curl -H 'Authorization: Bearer $(gcloud auth print-access-token)' -X POST -d '{"roles": $members, "preferredMemberKey": { "id": "${google_service_account.terraform_planner.email}" } }' https://cloudidentity.googleapis.com/v1beta1/${var.terraform_planners_google_group_id}/memberships"
-      curl -H 'Authorization: Bearer $(gcloud auth print-access-token)' -X POST -d '{"roles": $members, "preferredMemberKey": { "id": "${google_service_account.terraform_planner.email}" } }' https://cloudidentity.googleapis.com/v1beta1/${var.terraform_planners_google_group_id}/memberships"
+      curl --fail-with-body -H 'Authorization: Bearer $(gcloud auth print-access-token)' -X POST -d '{"roles": $members, "preferredMemberKey": { "id": "${google_service_account.terraform_planner.email}" } }' https://cloudidentity.googleapis.com/v1beta1/${var.terraform_planners_google_group_id}/memberships"
     EOT
   }
 }
