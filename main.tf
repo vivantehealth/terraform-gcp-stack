a100d61f82ceb89dbb5fe860c6a375dc9b4e60c1
Reese <3253971+figadore@users.noreply.github.com>
2024-11-19 17:32:20 +0000

    update group_memberships var to allow env groups and manager role

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "google_artifact_registry_repository_iam_member" "iac_admin" {
 data "google_cloud_identity_group_lookup" "group_lookup" {
   for_each = toset(var.group_memberships)
   group_key {
-    id = each.value
+    id = each.value.is_env_group ? "${each.value.group_name}-${var.env_id}@cylinderhealth.com" : "${each.value.group_name}@cylinderhealth.com"
   }
 }
 
@@ -143,8 +143,8 @@ resource "google_cloud_identity_group_membership" "custom_group_membership" {
   preferred_member_key {
     id = google_service_account.gha_iac.email
   }
-  // For the initial use case, only MEMBER is needed, but in the future, if the IaC SA needs to add a runtime SA to a group, MANAGER would be needed. This could be either hard-coded or configurable (would require updates to the structure of the group_memberships variable and associated stack yaml schema in gcp-env-terraform)
   roles {
-    name = "MEMBER"
+    // name = "MEMBER"
+    name = each.value.role == "member" ? "MEMBER" : "MANAGER"
   }
 }
